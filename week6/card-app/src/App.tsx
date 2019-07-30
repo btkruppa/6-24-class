0a2a762f75cf6fbe6a59ab3d57ab467f9e41e0de
Blake <btkruppa513@gmail.com>
2019-07-30 11:10:27 -0400

    added the rest of the react project setup

--- week6/card-app/src/App.tsx
+++ week6/card-app/src/App.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import './App.css';
 import { FirstComponent } from './components/first/first.component';
+import Second from './components/second/second.component';
+import Third from './components/third/third.component';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import NotFound from './components/not-found/not-found.component';
+import { NavComponent } from './components/app-nav/app-nav.component';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      App.tsx is the entry point to the application
-      <FirstComponent/>
-      <FirstComponent/>
-      <p>
-        We can import componenets wherever and as many times as we want
-      </p>
-      <FirstComponent/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavComponent />
+        {/* The switch will only render the first route to match */}
+        <Switch>
+          <Route path="/first" component={FirstComponent} />
+          <Route path="/second" component={Second} />
+          <Route path="/third" component={Third} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
