debe05db24fb0ffad7ca280361a48b8d74a33aa3
Blake <btkruppa513@gmail.com>
2019-08-16 14:36:54 -0400

    added cors to server

--- week8/IceCreamApi/src/main/java/com/revature/configuration/WebConfig.java
+++ week8/IceCreamApi/src/main/java/com/revature/configuration/WebConfig.java
@@ -50,7 +50,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // all endpoints in the api
-            .allowedOrigins("http://localhost:9001", "http://localhost:3000") // allow this origin
+            .allowedOrigins("http://localhost:9001", "http://localhost:3000", "http://1906-demo-blake.s3-website-us-east-1.amazonaws.com") // allow this origin
             .allowedMethods("PUT", "DELETE", "GET", "OPTIONS", "POST", "PATCH")
             .allowedHeaders("content-type", "header2", "Authorization")
 //            .exposedHeaders("header1", "header2")
