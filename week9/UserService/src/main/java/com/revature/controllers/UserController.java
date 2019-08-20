9a17b0c0f125edba7eb09830f8832d5b9d6dc752
Blake <btkruppa513@gmail.com>
2019-08-20 10:46:26 -0400

    added eureka and a service

---
+++ week9/UserService/src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.revature.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	private List<User> users = new ArrayList<>();
+
+	public UserController() {
+		users.add(new User(1, "Brenton", "Brenton@revature.com", "pass", "Larry Special"));
+		users.add(new User(2, "Phong", "Phong@revature.com", "pass", "Island"));
+		users.add(new User(3, "Carri", "Carri@revature.com", "pass", "Strawberry Gas"));
+		users.add(new User(4, "Blake", "Blake@revature.com", "pass", "1907"));
+	}
+
+	@GetMapping
+	public List<User> findAll() {
+		return users;
+	}
+}
---
+++ week9/UserService/src/test/java/com/revature/UserServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.revature;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
