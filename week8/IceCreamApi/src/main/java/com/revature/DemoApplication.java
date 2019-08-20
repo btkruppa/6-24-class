9bfd5d5391aea52e39d1c875dc3e4bd73561c96d
Blake <btkruppa513@gmail.com>
2019-08-20 11:24:47 -0400

    added my icecream service to register with eureka

--- week8/IceCreamApi/src/main/java/com/revature/DemoApplication.java
+++ week8/IceCreamApi/src/main/java/com/revature/DemoApplication.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,6 +10,7 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableDiscoveryClient
 @EnableSwagger2
 @SpringBootApplication
 public class DemoApplication {
